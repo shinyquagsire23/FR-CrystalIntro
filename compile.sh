66d5d559096dbef24e052d60ddfb9ac70675206a
shinyquagsire23 <mtinc2@gmail.com>
2013-08-14 14:09:54 -0700

    Forgot these things. :P

--- compile.sh
+++ compile.sh
@@ -4,5 +4,5 @@ arm-none-eabi-ld -o intro.o -T linker.lsc intro.out
 arm-none-eabi-objcopy -O binary intro.o toASM.bin
 rm intro.o
 rm intro.out
-java -jar AddASM.jar /home/maxamillion/ROMHacking/ASM/IntroTest/PEM.gba /home/maxamillion/ROMHacking/ASM/IntroTest/toASM.bin 0x9E0058
+java -jar AddASM.jar /home/maxamillion/ROMHacking/ASM/IntroTest/PEM.gba /home/maxamillion/ROMHacking/ASM/IntroTest/toASM.bin 0x9E0024
 #thumb toASM.asm
