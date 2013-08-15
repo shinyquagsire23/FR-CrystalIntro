402839393430c9077b2c037505c8f5329410e426
shinyquagsire23 <mtinc2@gmail.com>
2013-08-15 14:47:31 -0700

    Use make cont...

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 make
-java -jar AddASM.jar /home/maxamillion/ROMHacking/ASM/IntroTest/PEM.gba /home/maxamillion/ROMHacking/ASM/IntroTest/toASM.bin 0xAEFC34
+java -jar AddASM.jar /home/maxamillion/ROMHacking/ASM/IntroTest/PEM.gba /home/maxamillion/ROMHacking/ASM/IntroTest/intro.bin 0xAEFC34
 #thumb toASM.asm
